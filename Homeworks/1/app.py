2b44f798893f20629228419e5561cef258c8469d
Hasti Jalali <hastii.jalali@gmail.com>
2022-11-13 21:13:15 +0330

    update: postTracking

--- Homeworks/1/app.py
+++ Homeworks/1/app.py
@@ -1,23 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 import json
 from flask_cors import CORS, cross_origin
 import Database
 import Object_Storage as Obj
 import os
-
+import RabbitMQ_Send
 
 app = Flask(__name__)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
-db_dictionary = {
-    'HOST': "mysql-2a3e420a-hastii-2c07.aivencloud.com",
-    'PORT': 17752,
-    'USER': "avnadmin",
-    'PASSWORD': "AVNS_FLhMskMwQEoaul2OOjz",
-    'DATABASE': "defaultdb"
-}
 
-db = Database.Database(db_dictionary)
+
+db = Database.Database()
 
 # cors = CORS(app, resources={r'/api/*': {"origins": "*"}})
 
@@ -34,69 +28,46 @@ def addPost():
 
     email = request.form.get("email")
     description = request.form.get("description")
-
-    id = db.insert_data(email=email, description=description)
-    
     image = request.files['image']
     file_extension = os.path.splitext(image.filename)[1]
-    image.save(f'./Image Cache/{id}{file_extension}')
+    id = db.insert_data(email=email, description=description, extention=file_extension)
+    
+    file_path = f'./Image Cache/{id}{file_extension}'
+    image.save(file_path)
+    Obj.S3().upload_file(file_path)
+    os.remove(file_path) 
+    RabbitMQ_Send.rabbitMQ_send().send_message(f"{id}{file_extension}")
 
-    return f'Please Remember Your Tracking Code: {id}'
+    return f'لطفا کد پیگیری خود را به خاطر بسپارید: {id}'
 
+@app.route('/postTracking/', methods=['POST'])
+def postTracking():
 
-# @app.route('/api/movies/<id>')
-# @cross_origin()
-# def get_movie(id):
-#     movie = Movie_Top_Chart.query.get_or_404(id)
-#     output = [{
-#             'name': movie.name,
-#             'id': str(movie.id),
-#             'address1': movie.address1,
-#             'address2': movie.address2,
-#             'year': str(movie.year),
-#             'director': movie.director,
-#             'score': str(movie.score)
-#         }]
-#     # print(output)
-#     # for movie in movies:
-#     #     movie_data = {'name': movie.name, 'description': movie.description}
-#     #     output.append(movie_data)
-#     return json.dumps(output)
+    id = request.form.get("id")
 
-# @app.route('/api/topPicks')
-# @cross_origin()
-# def get_top_picks():
-#     movies = Other_Movie.query.all()
-#     output = []
-#     for movie in movies:
-#         if movie.type == 1:
-#             output.append({
-#                 'name': movie.name,
-#                 'id': str(movie.id),
-#                 'address': movie.address,
-#                 'year': str(movie.year),
-#             })
-#     return json.dumps(output)
+    row = db.select_by_id(id)[0]
+    # print(row)
+    '''
+    row[0] = id
+    row[1] = description
+    row[2] = email
+    row[3] = state
+    row[4] = category
+    row[6] = extention
+    '''
+    if row[3] == 0:
+        return f'آگهی شما با آیدی {id} در صف بررسی است.'
 
-# @app.route('/api/fanFav')
-# @cross_origin()
-# def get_fan_fav():
-#     movies = Other_Movie.query.all()
-#     output = []
-#     for movie in movies:
-#         if movie.type == 2:
-#             output.append({
-#                 'name': movie.name,
-#                 'id': str(movie.id),
-#                 'address': movie.address,
-#                 'year': str(movie.year),
-#             })
-#     return json.dumps(output)
+    if row[3] == 1:
+        return f'آگهی شما با آیدی {id} رد شده است.'
+    
+    elif row[3] == 2:
+        result = f"توضیحات: {row[1]}\n"
+        result += f"دسته‌بندی {row[4]}\n"
+        result += f"وضعیت {row[3]}\n"
+        extention = row[6]
+        file_name = Obj.S3().download_file(object_name=id, extention=extention)
+        img = open(file_name, 'rb')
+        # return make_response(result, img)
+        return send_file(img, mimetype='image/jpeg'), result
 
-
-# @app.route('/api/stream/<id>')
-# @cross_origin()
-# def start_stream(id):
-#     # print('id', id)
-#     # output = {'address': (f'./Videos/TopChartMpd/{id}/{id}.mpd')}
-#     return f'./Videos/TopChartMpd/{id}/{id}.mpd'
