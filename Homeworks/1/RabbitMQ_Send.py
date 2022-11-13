271df8e3de2f460561b580c483e6ee5bc9b17a44
Hasti Jalali <hastii.jalali@gmail.com>
2022-11-13 21:19:13 +0330

    fix bugs

--- Homeworks/1/RabbitMQ_Send.py
+++ Homeworks/1/RabbitMQ_Send.py
@@ -1,21 +1,21 @@
 import pika
 
+
 # Create a connection to the RabbitMQ server
 class rabbitMQ_send():
-    def __init__(self, AMQP_URL, ROUTING_KEY):
-        self.AMQP_URL = AMQP_URL
-        self.ROUTING_KEY = ROUTING_KEY
+    def __init__(self):
+        self.AMQP_URL = "amqps://wqzfdbgv:H0k0rWOPf6vTBaFCgJ35vZgWn657nAQZ@albatross.rmq.cloudamqp.com/wqzfdbgv"
+        self.ROUTING_KEY = 'hello'
         self.connection = pika.BlockingConnection(pika.URLParameters(self.AMQP_URL))
         self.channel = self.connection.channel()
 
     def send_message(self, message):
-        self.channel.queue_declare(queue=message)
+        self.channel.queue_declare(queue='hello')
 
         self.channel.basic_publish(exchange='', routing_key=self.ROUTING_KEY, body=message)
         print(f" sent {message}")
     
 # test
-AMQP_URL = "amqps://wqzfdbgv:H0k0rWOPf6vTBaFCgJ35vZgWn657nAQZ@albatross.rmq.cloudamqp.com/wqzfdbgv"
-ROUTING_KEY = "hello"
-message = "hello world"
-rabbitMQ_send(AMQP_URL, ROUTING_KEY).send_message(message)
+
+# message = "hello world"
+# rabbitMQ_send().send_message(message)
