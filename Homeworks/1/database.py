f5044e5af7d336bc66a6da25823ed175fb546332
Hasti Jalali <hastii.jalali@gmail.com>
2022-11-13 21:21:43 +0330

    change email and add some sql function to database:

--- Homeworks/1/database.py
+++ Homeworks/1/database.py
@@ -30,13 +30,13 @@ import mysql.connector
 '''
 class Database():
 
-    def __init__(self, HOST, PORT, USER, PASSWORD, DATABASE):
+    def __init__(self):
         self.mydb = mysql.connector.connect(
-            host = HOST,
-            port = PORT,
-            user = USER,
-            password = PASSWORD,
-            database = DATABASE
+            host = "mysql-2a3e420a-hastii-2c07.aivencloud.com",
+            port = 17752,
+            user = "avnadmin",
+            password = "AVNS_FLhMskMwQEoaul2OOjz",
+            database = "defaultdb"
         )
         print(self.mydb)
     
@@ -46,11 +46,10 @@ class Database():
         mycursor.execute(query)
         self.mydb.commit()
     
-    def insert_data(self, email, description):
+    def insert_data(self, email, description, extention):
         mycursor = self.mydb.cursor()
-        table_name = 'Advertisement'
-        sql = f"INSERT INTO {table_name} (Description, Email, State) VALUES (%s, %s, %d)"
-        val = (email, description, 0)
+        sql = "INSERT INTO Advertisement (Description, Email, Extention, State) VALUES (%s, %s, %s, 0)"
+        val = (description, email, extention)
         mycursor.execute(sql, val)
 
         self.mydb.commit()
@@ -60,10 +59,8 @@ class Database():
     
     def update_data(self, id, state, category=''):
         mycursor = self.mydb.cursor()
-        table_name = 'Advertisement'
-        sql = f"UPDATE {table_name} SET (State, Category) VALUES (%d, %s) WHERE Id = %d"
-        val = (state, category, id)
-        mycursor.execute(sql, val)
+        sql = f"UPDATE Advertisement SET State = {state}, Category = '{category}' WHERE Id = {id}"
+        mycursor.execute(sql)
 
         self.mydb.commit()
 
@@ -76,17 +73,41 @@ class Database():
         for x in mycursor:
             print(x)
     
-    def select_rows(self, table_name):
+    def select_by_id(self, id):
         mycursor = self.mydb.cursor()
-        mycursor.execute(f"SELECT * FROM {table_name}")
+        mycursor.execute(f"SELECT * FROM Advertisement WHERE ID={id}")
 
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
+        return myresult
+
+    def alter_table(self):
+        mycursor = self.mydb.cursor()
+        mycursor.execute(f"ALTER TABLE Advertisement ADD extention varchar(10)")
+        self.mydb.commit()
+    
+    def delete_row(self, id):
+        mycursor = self.mydb.cursor()
+        mycursor.execute(f"DELETE FROM Advertisement WHERE Id = {id}")
+        self.mydb.commit()
+
+    def get_email(self, id):
+        mycursor = self.mydb.cursor()
+        mycursor.execute(f"SELECT Email FROM Advertisement WHERE Id = {id}")
+
+        myresult = mycursor.fetchall()
+        return myresult[0][0]
+        
+
 
 # db = Database(HOST, PORT, USER, PASSWORD, DATABASE)
 # # db.select_tables()
 # # db.creat_table(create_table_query)
-# db.select_rows('Advertisement')
+# Database().insert_data('hastii@gmail.com', 'test')
+# for id in range(1, 6):
+#     Database().delete_row(id)
+
+# Database().select_by_id(6)
--- Homeworks/1/email.py
+++ Homeworks/1/email.py
@@ -1,27 +1,28 @@
 import requests
 
-# Data start:
-DOMAIN = "sandboxea9c004cefa840e19efa1b740a8263d0.mailgun.org"
-API_KEY = "e8f00bed28567c185c4a79f1c54f0d99-48c092ba-2aa2328c"
 
+class SendEmail():
+    def __init__(self):
+        self.DOMAIN = "sandboxea9c004cefa840e19efa1b740a8263d0.mailgun.org"
+        self.API_KEY = "e8f00bed28567c185c4a79f1c54f0d99-48c092ba-2aa2328c"
 
-
-# End
-
-class Email():
-    def send_email(email_address, text, subject):
+    def send_message(self, email, subject, text):
         return requests.post(
-            f"https://api.mailgun.net/v3/{DOMAIN}/messages",
-            auth=("api", API_KEY),
-            data={"from": f"{DOMAIN}>",
-                "to": [email_address],
-                "subject": subject,
-                "text": text})
+            f"https://api.mailgun.net/v3/{self.DOMAIN}/messages",
+            auth=("api", self.API_KEY),
+            data={"from": f"<mailgun@{self.DOMAIN}>",
+                  "to": [email],
+                  "subject": subject,
+                  "text": text})
 
 
-# test
-EMAIL_ADDRESS = "hastii.jalali@gmail.com"
-TEXT = "Your ad has been accepted!"
-SUBJECT = "Cloud Computing HW1"
-response = Email().send_simple_message(EMAIL_ADDRESS, SUBJECT, TEXT)
-print(response.json())
+# EMAIL_ADDRESS = "hastii.jalali@gmail.com"
+# TEXT = "testtttttt"
+# SUBJECT = "Cloud Computing HW1"
+
+# email_response = SendEmail().send_message(EMAIL_ADDRESS, SUBJECT, TEXT)
+
+# print(email_response)
+
+
+# print(image.tagging_obj())
