ec99a855cbd5c10860843824154e06dba45b4360
Hasti Jalali <hastii.jalali@gmail.com>
2022-11-13 21:19:56 +0330

    check that a photo is vehicle or not

--- Homeworks/1/Image_Tagging.py
+++ Homeworks/1/Image_Tagging.py
@@ -1,27 +1,28 @@
 import requests
-# Data start:
-API_KEY = 'acc_f596c8c6fe207a0'
-API_SECRET = 'b117d89284326d807458a878ceaa29e7'
-IMAGE_URL = 'https://wallpapercave.com/wp/wp3503654.jpg'
-
-# End
 
 class ImageTagging():
-    def __init__(self, API_KEY, API_SECRET):
-        self.API_KEY = API_KEY
-        self.API_SECRET = API_SECRET
+    def __init__(self):
+        self.API_KEY = 'acc_f596c8c6fe207a0'
+        self.API_SECRET = 'b117d89284326d807458a878ceaa29e7'
 
-    def get_tags(self, image_url):
-        response = requests.get(
-            'https://api.imagga.com/v2/tags?image_url=%s' % image_url,
-                auth=(self.API_KEY, self.API_SECRET))
+    def get_tags(self, image_path):
+        response = requests.post(
+            'https://api.imagga.com/v2/tags',
+            auth=(self.API_KEY, self.API_SECRET),
+            files={'image': open(image_path, 'rb')})
+        # print(response.json())
+        check_vehicle = False
+        for i in response.json()['result']['tags']:
+            if i['tag']['en'] == 'vehicle':
+                check_vehicle = True
+                break
         tags = response.json()['result']['tags']
         for tag in tags:
             confidence = tag['confidence']
             tag_name = tag['tag']['en']
-            print(f'Confidence: {confidence}, tag: {tag_name}')
-            return tag_name, confidence
+            print(f'Confidence: {confidence}, tag: {tag_name}, is_vehicle: {check_vehicle}')
+            return tag_name, check_vehicle
 
 
 # test
-tag_name, confidence = ImageTagging(API_KEY, API_SECRET).get_tags(IMAGE_URL)
+# tag_name, confidence, _ = ImageTagging().get_tags('./Image Cache/1.jpg')
