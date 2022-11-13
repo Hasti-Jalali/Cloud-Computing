d696b8f5f2743fabf1cb02e45cc436cde4386a71
Hasti Jalali <hastii.jalali@gmail.com>
2022-11-13 21:17:58 +0330

    update: callback

--- Homeworks/1/RabbitMQ_Recieve.py
+++ Homeworks/1/RabbitMQ_Recieve.py
@@ -1,15 +1,20 @@
 import pika
 import sys
 import os
+import Object_Storage as Obj
+import Image_Tagging as Tag
+import Database
+import Email
 
+db = Database.Database()
 
 class RabbitMQ_Receive():
-    def __init__(self, AMQP_URL, queue):
-        self.AMQP_URL = AMQP_URL
+    def __init__(self):
+        self.AMQP_URL = "amqps://wqzfdbgv:H0k0rWOPf6vTBaFCgJ35vZgWn657nAQZ@albatross.rmq.cloudamqp.com/wqzfdbgv"
         self.connection = pika.BlockingConnection(
             pika.URLParameters(self.AMQP_URL))
         self.channel = self.connection.channel()
-        self.queue = queue
+        self.queue = 'hello'
         
 
     def receive(self):
@@ -17,14 +22,37 @@ class RabbitMQ_Receive():
 
         def callback(ch, method, properties, body):
             print(" [x] Received %r" % body)
+            body = body.decode('utf-8')
+            id = int(os.path.splitext(body)[0])
+            extention = os.path.splitext(body)[1]
+            file_name = body
+            file_name = Obj.S3().download_file(id, extention)
+            tag, is_vehicle = Tag.ImageTagging().get_tags(file_name)
+            if is_vehicle:
+                state = 2
+                db.update_data(id=id, state=state, category=tag)
+                email = db.get_email(id)
+                text = ' تبلیغ شما تایید شد'
+                subject = 'تایید تبلیغ'
+                Email.SendEmail().send_message(email, text, subject)
+
+            if is_vehicle is False:
+                state = 1
+                db.update_data(id=id, state=state, category=tag)
+                email = db.get_email(id)
+                text = 'تبلیغ شما تایید نشد'
+                subject = 'رد تبلیغ'
+                Email.SendEmail().send_message(email, text, subject)
+            
+            print('Email Sent!')
+
+            # print(" [x] Received %r" % body)
+        
         self.channel.basic_consume(
             queue=self.queue, on_message_callback=callback, auto_ack=True)
         print(' [*] Waiting for messages. To exit press CTRL+C')
         self.channel.start_consuming()
 
 
-AMQP_URL = "amqps://wqzfdbgv:H0k0rWOPf6vTBaFCgJ35vZgWn657nAQZ@albatross.rmq.cloudamqp.com/wqzfdbgv"
 
-
-queue = "hello"
-print(RabbitMQ_Receive(AMQP_URL, queue).receive())
+print(RabbitMQ_Receive().receive())
